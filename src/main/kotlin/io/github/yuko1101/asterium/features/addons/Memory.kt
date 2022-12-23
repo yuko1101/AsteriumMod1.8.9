f6330e55bc7b371acd06e3318e6e1ef145a276e8
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2022-12-23 15:09:20 +0900

    add memory system for reloading addons

---
+++ src/main/kotlin/io/github/yuko1101/asterium/features/addons/Memory.kt
@@ -0,0 +1,43 @@
+package io.github.yuko1101.asterium.features.addons
+
+import java.util.*
+
+class Memory(private val addon: AddonMetaData) {
+    
+    companion object {
+        val data = mutableMapOf<UUID, MutableMap<String, Any>>()
+    }
+
+    init {
+        if (!data.containsKey(addon.uuid)) data[addon.uuid] = mutableMapOf()
+    }
+
+    private val addonMemory: MutableMap<String, Any>
+        get() = data[addon.uuid]!!
+
+    /** @return Whether the value set successfully. */
+    fun set(key: String, value: Any): Boolean {
+        if (addonMemory.containsKey(key)) {
+            return false
+        }
+        addonMemory[key] = value
+        return true
+    }
+
+    /** @return Returns `true` if the key has already been set and overwrites the value. Returns `false` if the key has not been set. */
+    fun overwrite(key: String, value: Any): Boolean {
+        val hasKey = addonMemory.containsKey(key)
+        addonMemory[key] = value
+        return hasKey
+    }
+
+    fun get(key: String): Any? {
+        return addonMemory[key]
+    }
+
+    fun has(key: String): Boolean {
+        return addonMemory.containsKey(key)
+    }
+
+
+}

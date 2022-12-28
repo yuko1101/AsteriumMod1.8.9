0b275f6df53c0c028e2429dd067212913eb7d0b8
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2022-12-28 16:07:23 +0900

    nullable params

--- src/main/kotlin/io/github/yuko1101/asterium/utils/minecraft/ChatLib.kt
+++ src/main/kotlin/io/github/yuko1101/asterium/utils/minecraft/ChatLib.kt
@@ -7,17 +7,21 @@ import net.minecraft.util.IChatComponent
 object ChatLib {
     private val mc = Minecraft.getMinecraft()
     fun chat(text: String?) {
+        if (text == null) return
         mc.thePlayer.addChatMessage(ChatComponentText(text))
     }
 
     fun say(text: String?) {
+        if (text == null) return
         mc.thePlayer.sendChatMessage(text)
     }
 
-    fun command(text: String) {
+    fun command(text: String?) {
+        if (text == null) return
         mc.thePlayer.sendChatMessage("/$text")
     }
-    fun chatComponent(component: IChatComponent) {
+    fun chatComponent(component: IChatComponent?) {
+        if (component == null) return
         mc.thePlayer.addChatComponentMessage(component)
     }
 }
